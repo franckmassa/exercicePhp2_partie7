e4811398945cd760671d2af6fbedd262057a2af1
massa <massafranck@gmail.com>
2018-08-22 21:52:43 +0200

    corrigé

--- user.php
+++ user.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice2</title>
-</head>
-<body>
-  <p>
-    
-  </p>
-</body>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice2</title>
+    </head>
+    <body>
+        <p>
+
+        </p>
+    </body>
 </html>
